92a57ff2a589b2145d16a4e5a2494d22d19bb711
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-04-23 18:04:10 +0300

    This will add the class attributes

--- src/main/java/models/CaesarEncoderDecorder.java
+++ src/main/java/models/CaesarEncoderDecorder.java
@@ -2,4 +2,16 @@ package models;
 
 public class CaesarEncoderDecorder {
 
+    private static int key = 25;
+    private static String encryptMessage;
+    private static String dencryptMessage;
+    Character[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
 }
+
+
+
+
+
+
+
+
