f05d59e328bf926e7a0e9d4bd1ce6461246c1070
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-04-24 10:17:53 +0300

    this will add the whole decrypted Test class

--- src/test/java/models/decryptedCharacterTest.java
+++ src/test/java/models/decryptedCharacterTest.java
@@ -5,20 +5,20 @@ import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class decryptedCharacterTest {
-//    @Test
-//    public void decryptedCharacterText_returnsCharPlusThreePositions_F() {
-//        decryptedCharacter decryptedCharacter = new decryptedCharacter();
-//        Character expected = 'I';
-//        assertEquals(expected, decryptedCharacter.decryptedCharacterText('F'));
-//
-//    }
-//    @Test
-//    public void decryptedCharacterText_returnsCharPlusThreePositions_K() {
-//        decryptedCharacter decryptedCharacter = new decryptedCharacter();
-//        Character expected = 'N';
-//        assertEquals(expected, decryptedCharacter.decryptedCharacterText('K'));
-//
-//    }
+    @Test
+    public void decryptedCharacterText_returnsCharPlusThreePositions_F() {
+        decryptedCharacter decryptedCharacter = new decryptedCharacter();
+        Character expected = 'I';
+        assertEquals(expected, decryptedCharacter.decryptedCharacterText('F'));
+
+    }
+    @Test
+    public void decryptedCharacterText_returnsCharPlusThreePositions_K() {
+        decryptedCharacter decryptedCharacter = new decryptedCharacter();
+        Character expected = 'N';
+        assertEquals(expected, decryptedCharacter.decryptedCharacterText('K'));
+
+    }
 
     @Test
     public void decryptedCharacterText_returnsCharPlusThreePositions_BOY() {
