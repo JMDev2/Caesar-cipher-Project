98eb1a3ef4e75316ee344722eaabe0d64e486660
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-04-25 20:00:01 +0300

    This will add the readme and licence files

--- src/test/java/models/decryptedCharacterTest.java
+++ src/test/java/models/decryptedCharacterTest.java
@@ -6,10 +6,10 @@ import static junit.framework.TestCase.assertEquals;
 
 public class decryptedCharacterTest {
     @Test
-    public void decryptedCharacter_returnsCharMinusThreePositions_F() {
+    public void decryptedCharacter_returnsCharMinusThreePositions_QEB() {
         decryptedCharacter decryptedCharacter = new decryptedCharacter();
-        String expected = "F";
-        assertEquals(expected, decryptedCharacter.decryptedCharacter("F", 3));
+        String expected = "THE";
+        assertEquals(expected, decryptedCharacter.decryptedCharacter("QEB", 3));
 
     }
 }
